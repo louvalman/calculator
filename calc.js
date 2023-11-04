a1c326ab88637ed9dd24b5505e29052a90750e22
louvalman <louisdyrhauge@hotmail.com>
2023-11-04 23:58:51 +0100

    basic calc layout established in html and flexbox

--- calc.js
+++ calc.js
@@ -1,3 +1,9 @@
+// Calculator variables
+const num1 = 0;
+const num2 = 0;
+const op = 0;
+
+// Calculator functions
 const add = function (num1, num2) {
   return num1 + num2;
 };
@@ -10,8 +16,12 @@ const sum = function (array) {
   return array.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function (array) {
-  return array.reduce((acc, cur) => acc * cur);
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+  return num1 / num2;
 };
 
 const power = function (num, power) {
@@ -31,3 +41,22 @@ const factorial = function (num) {
   }
   return result;
 };
+
+// Operation function
+
+const operate = function (op, num1, num2) {
+  if (op === '+') {
+    return add(num1, num2);
+  } else if (op === '-') {
+    return subtract(num1, num2);
+  } else if (op === '*') {
+    return multiply(num1, num2);
+  } else if (op === '/') {
+    return divide(num1, num2);
+  }
+};
+
+console.log(operate('+', 2, 1));
+console.log(operate('-', 2, 1));
+console.log(operate('*', 1, 2));
+console.log(operate('/', 1, 2));
