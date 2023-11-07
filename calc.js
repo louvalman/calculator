81eb79df88176fce7f584a2399d9648344a63a64
louvalman <louisdyrhauge@hotmail.com>
2023-11-07 22:39:15 +0100

    added minidisplay to show status of calc

--- calc.js
+++ calc.js
@@ -5,10 +5,12 @@ let op = 0;
 let displayValue = 0;
 
 // Display
-const display = document.querySelector('.display');
+const right = document.querySelector('.right');
 const div = document.createElement('div');
+const miniDisplay = document.querySelector('.mini-display');
+const p = document.createElement('p');
 
-display.appendChild(div).textContent = displayValue;
+right.appendChild(div).textContent = displayValue;
 
 // Calculator functions
 // Add
@@ -58,58 +60,65 @@ const operate = function () {
 const equalButton = document.querySelector('.equal-button');
 equalButton.addEventListener('click', function () {
   if (num2 === 0) {
-    num2 = displayValue;
+    num2 = Number(displayValue);
     operate(op, num1, num2);
-    display.appendChild(div).textContent = displayValue;
+    right.appendChild(div).textContent = displayValue;
   }
 });
 
-// Clear function
+// Clear function (AC)
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', function () {
   num1 = 0;
   num2 = 0;
   op = 0;
   displayValue = 0;
-  display.appendChild(div).textContent = displayValue;
+  right.appendChild(div).textContent = displayValue;
+  miniDisplay.appendChild(p).textContent = '';
 });
 
-// Dot (comma) button function
-
+// Dot (comma) button functionionality
+// Check if comma is present
 const hasComma = (num) => {
   const numString = num.toString();
   return numString.includes('.');
 };
 
+// Make sure only one comma can exist
 const dotButton = document.querySelector('.dot-button');
 dotButton.addEventListener('click', function () {
   if (!hasComma(displayValue)) {
     displayValue += '.';
-    display.appendChild(div).textContent = displayValue;
+    right.appendChild(div).textContent = displayValue;
   }
 });
 
 // Populate display when number or operate buttons are pressed
-
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
   button.addEventListener('click', function () {
     const operator = button.getAttribute('data-operator');
     const number = button.getAttribute('data-number');
-    // Check if the button has a data-operator attribute
+    // Check if the button has a data-operator attribute and populate display
     if (operator) {
       displayValue = '';
-      display.appendChild(div).textContent = displayValue;
+      right.appendChild(div).textContent = displayValue;
     }
-    // Check if the button has a data-number attribute
+    // Populate mini-display when operator is clicked
+    if (num1 !== 0 && num2 !== 0 && op !== 0) {
+      miniDisplay.appendChild(p).textContent = `${num1} ${op} ${num2}`;
+    } else if (num1 !== 0) {
+      miniDisplay.appendChild(p).textContent = `${num1} ${op}`;
+    }
+    // Check if the button has a data-number attribute and populate display
     if (number) {
       if (displayValue !== 0 && displayValue !== '+') {
         displayValue += number;
       } else {
         displayValue = number;
       }
-      display.appendChild(div).textContent = displayValue;
+      right.appendChild(div).textContent = displayValue;
     }
   });
 });
