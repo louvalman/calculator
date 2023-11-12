16d63c8b31075ddf0f3d2eef870d41b061113c5b
louvalman <louisdyrhauge@hotmail.com>
2023-11-12 22:32:44 +0100

    separated logic so that number and operation handling is two functions, included display updates here, fixed bug with chaining operations

--- calc.js
+++ calc.js
@@ -1,8 +1,11 @@
+// Get buttons
+const buttons = document.querySelectorAll('button');
+
 // Calculator variables
 let num1 = 0;
 let num2 = 0;
 let op = 0;
-let displayValue = 0;
+let displayValue = '0';
 
 // Display
 const right = document.querySelector('.right');
@@ -10,49 +13,81 @@ const div = document.createElement('div');
 const miniDisplay = document.querySelector('.mini-display');
 const p = document.createElement('p');
 
-right.appendChild(div).textContent = displayValue;
+// Declare update display function and call it once to show the initial displayvalue of '0'
+const updateDisplay = () => {
+  right.appendChild(div).textContent = displayValue;
+};
 
-// Calculator functions
-// Add
-const addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', function () {
+updateDisplay();
+
+// Update variables when operator or number is pressed
+function handleOperator(operator) {
   if (num1 !== 0 && num2 === 0) {
-    // OBS virker ikke (lav 33 + 44 + 55 (equal fix))
-    op = '+';
-    return;
-  } else if (num1 !== 0 && num2 !== 0) {
+    // if user is trying to chain operations
+    const parts = displayValue.split(' ');
+    const secondOperand = parts[2];
+    num2 = Number(secondOperand); // set num2 to second part of displayValue
+    miniDisplay.appendChild(p).textContent = `${num1} ${op} ${num2}`; // populate miniDisplay with the previous 'num1 op num2'
+    displayValue = operate(num1, num2, op);
     num1 = Number(displayValue);
     num2 = 0;
-    op = '+';
+    op = operator;
+    displayValue += ` ${op} `;
+    updateDisplay();
+  } else if (!displayValue.split('').includes(operator)) {
+    op = operator;
+    num1 = Number(displayValue);
+    num2 = 0;
+    displayValue += ` ${op} `;
   }
+}
+
+function handleNumber(number) {
+  if (displayValue !== '0') {
+    displayValue += number;
+  } else {
+    displayValue = number;
+  }
+}
+
+// Determine how to handle input depending on which type of button is pressed and update display accordingly
+buttons.forEach((button) => {
+  button.addEventListener('click', function () {
+    const operator = button.getAttribute('data-operator');
+    const number = button.getAttribute('data-number');
+
+    if (operator) {
+      handleOperator(operator);
+    } else if (number) {
+      handleNumber(number);
+    }
+    updateDisplay();
+  });
 });
 
+// Calculator functions and operate function that takes uses calc functions with the input numbers and operators
 const add = function (num1, num2) {
   return Number(num1) + Number(num2);
 };
 
 // Subtract
 const subtract = function (num1, num2) {
-  return num1 - num2;
+  return Number(num1) - Number(num2);
 };
 
 // Multiply
 const multiply = function (num1, num2) {
-  return num1 * num2;
+  return Number(num1) * Number(num2);
 };
 
 // Divide
 const divide = function (num1, num2) {
-  return num1 / num2;
+  return Number(num1) / Number(num2);
 };
 
 // Operation function
-const operate = function () {
+const operate = function (num1, num2, op) {
   if (op === '+') {
-    displayValue = add(num1, num2);
-    right.appendChild(div).textContent = displayValue;
-    num1 = displayValue;
-    num2 = 0;
     return add(num1, num2);
   } else if (op === '-') {
     return subtract(num1, num2);
@@ -66,13 +101,13 @@ const operate = function () {
 // Equal function
 const equalButton = document.querySelector('.equal-button');
 equalButton.addEventListener('click', function () {
-  if (num2 === 0 && displayValue !== '') {
+  if (num1 !== 0 && num2 === 0 && displayValue !== '0') {
     const parts = displayValue.split(' ');
     const secondOperand = parts[2];
     num2 = Number(secondOperand);
     miniDisplay.appendChild(p).textContent = `${num1} ${op} ${num2}`;
-    operate(op, num1, num2);
-    right.appendChild(div).textContent = displayValue;
+    displayValue = operate(num1, num2, op);
+    updateDisplay();
   }
 });
 
@@ -82,9 +117,9 @@ clearButton.addEventListener('click', function () {
   num1 = 0;
   num2 = 0;
   op = 0;
-  displayValue = 0;
-  right.appendChild(div).textContent = displayValue;
+  displayValue = '0';
   miniDisplay.appendChild(p).textContent = '';
+  updateDisplay();
 });
 
 // Dot (comma) button functionionality
@@ -101,35 +136,4 @@ dotButton.addEventListener('click', function () {
     displayValue += '.';
     right.appendChild(div).textContent = displayValue;
   }
-});
-
-// Populate display when number or operate buttons are pressed
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', function () {
-    const operator = button.getAttribute('data-operator');
-    const number = button.getAttribute('data-number');
-    const equal = button.getAttribute('data-equal');
-    // Check if the button has a data-operator attribute and populate display
-    if (operator) {
-      if (Number(displayValue) === num1) {
-        displayValue = `${num1} ${op} `;
-        miniDisplay.appendChild(p).textContent = `Ans ${num1}`;
-      } else if (num1 !== 0 && num2 === 0) {
-        displayValue += `${num1} ${op} `;
-      }
-      right.appendChild(div).textContent = displayValue;
-    }
-    // Populate mini-display when operator is clicked
-    // Check if the button has a data-number attribute and populate display
-    if (number) {
-      if (displayValue !== 0) {
-        displayValue += number;
-      } else {
-        displayValue = number;
-      }
-      right.appendChild(div).textContent = displayValue;
-    }
-  });
 });
